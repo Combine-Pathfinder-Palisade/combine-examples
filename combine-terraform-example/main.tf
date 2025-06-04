ebe3349c9ec87e8c4ceffd2dc5be96f9a352ab01
Daniel Sabbagh <dsabbagh@sequoiainc.com>
2025-06-04 16:42:32 -0400

    rename files

--- combine-terraform-example/main.tf
+++ combine-terraform-example/main.tf
@@ -1,7 +1,5 @@
-# main.tf
-```
-resource "aws_sqs_queue" "renderedai_terraform_queue" {
-  name                        = "renderedai_terraform_queue.fifo"
+resource "aws_sqs_queue" "customer_terraform_queue" {
+  name                        = "customer_terraform_queue.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
   policy = jsonencode({
@@ -16,12 +14,11 @@ resource "aws_sqs_queue" "renderedai_terraform_queue" {
       },
       "Condition": {
         "ArnEquals": {
-          "aws:SourceArn": "arn:aws-iso:sns:us-iso-east-1:193220141526:foo"
+          "aws:SourceArn": "arn:aws-iso:sns:us-iso-east-1:123456789012:foo"
         }
       },
-      "Resource": "arn:aws-iso:sqs:us-iso-east-1:663117128738:renderedai_terraform_queue.fifo"
+      "Resource": "arn:aws-iso:sqs:us-iso-east-1:123456789012:renderedai_terraform_queue.fifo"
     }
   ]
 })
 }
-```
--- combine-terraform-example/providers.tf
+++ combine-terraform-example/providers.tf
@@ -1,5 +1,4 @@
-# providers.tf
-```
+
 terraform {
   required_providers {
     aws = {
@@ -9,11 +8,12 @@ terraform {
   }
 }
 
+# access key and secret key can be sourced from the CAP api, or sourced via 
+# environment variables, see https://registry.terraform.io/providers/hashicorp/aws/latest/docs#provider-configuration
 provider "aws" {
-  region     = "us-iso-east-1"
+  region     = "us-iso-east-1" # change if your emulated region is different
   access_key = "<obtained-from-CAP-API>"
   secret_key = "<obtained-from-CAP-API>"
   skip_metadata_api_check = true
   custom_ca_bundle = /path/to/ca-chain.cert.pem
 }
-```
